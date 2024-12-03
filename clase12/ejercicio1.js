fedbf8b8dc567f0a356cb0013c0648a96eb1ce03
Matias-Joel-Silva <matasilva777@gmail.com>
2024-12-03 18:59:26 -0300

    clase 12 - ej 1

---
+++ clase12/ejercicio1.js
@@ -0,0 +1,29 @@
+let dia = "domingo";
+switch (dia) {
+    case "lunes":
+        console.log("Hoy es lunes, empieza la semana con energia");
+        break;
+    case "martes":
+        console.log("Hoy es martes sigue adelante");
+        break;
+    case "miercoles":
+        console.log("Hoy es miercoles, mitad de semana");
+        break;
+    case "jueves":
+        console.log("Hoy es jueves, casi es viernes");
+        break;
+    case "viernes":
+        console.log("Hoy es viernes, el fin de semana esta cerca");
+        break;
+    case "sabado":
+        console.log("Hoy es sabado, disfruta tu dia");
+        break;
+    case "domingo":
+        console.log("Hoy es domingo, dia de descanso");
+        break;
+        
+    default:
+        console.log("Error al escribir el dia");
+        
+        break;
+}
